3bc2eb8e7256fa9e7fe9d28b666b618f8d8c4188
TejasBh <special_tbhat@hotmail.com>
2014-10-09 22:32:12 +1100

    Added two functions. Still rusty

--- main2.pl
+++ main2.pl
@@ -3,3 +3,37 @@
 use strict;
 use warnings;
 use LWP::Simple;
+
+sub get_lyrics {
+	my $html = get "http://www.azlyrics.com/lyrics/breakingbenjamin/naturallife.html";
+
+	#clean
+	$html =~ s/.*start of lyrics//s;
+	$html =~ s/end of lyrics.*//s;
+	$html =~ s/<[^>]*>/ /g;
+	$html =~ s/[,!]//g;
+	$html =~ s/\n//g;
+	return $html;
+}
+
+sub count_words {
+	my %wordCount = ();
+	my $lyrics = get_lyrics();
+
+	my @words = split(/\s+/, $lyrics);
+	foreach (@words) {
+		my $word = $_;
+		if (exists $wordCount{$word}) {
+			$wordCount{$word}++;
+		} else {
+			$wordCount{$word} = 1;
+		}
+    }
+
+    foreach my $name (keys %wordCount) {
+    	printf "%-8s %s\n", $name, $wordCount{$name};
+    }
+
+}
+
+count_words();
